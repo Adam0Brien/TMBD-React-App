f1759ed77403560eda1416fd3a8c46a8aeac246b
Adam0Brien <20093460@mail.wit.ie>
2022-10-16 00:12:57 +0100

    Added routing and live data from API

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,11 +1,58 @@
-import React from "react";
+//// OLD CODE ////
+
+// import React from "react";
+// import Header from "../components/headerMovieList";
+// import FilterCard from "../components/filterMoviesCard";
+// import MovieList from "../components/movieList";
+// import Grid from "@mui/material/Grid";
+
+// const MovieListPage = (props) => {
+//   const movies = props.movies;
+
+//   return (
+//     <Grid container sx={{ padding: "20px" }}>
+//       <Grid item xs={12}>
+//         <Header title={"Home Page"} />
+//       </Grid>
+//       <Grid item container spacing={5}>
+//         <Grid key="find" item xs={12} sm={6} md={4} lg={3} xl={2}>
+//           <FilterCard />
+//         </Grid>
+//         <MovieList movies={movies}></MovieList>
+//       </Grid>
+//     </Grid>
+//   );
+// };
+// export default MovieListPage;
+ 
+
+////New Code////
+
+import React, { useState, useEffect } from "react"; 
 import Header from "../components/headerMovieList";
 import FilterCard from "../components/filterMoviesCard";
 import MovieList from "../components/movieList";
 import Grid from "@mui/material/Grid";
 
 const MovieListPage = (props) => {
-  const movies = props.movies;
+  const [movies, setMovies] = useState([]);
+  const key = "80c07a94e81a6142e7dd02eaef22d442"
+
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?api_key="+key+"&language=en-US&include_adult=false&page=1"
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        return json.results;
+      })
+      .then((movies) => {
+        setMovies(movies);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
 
   return (
     <Grid container sx={{ padding: "20px" }}>
