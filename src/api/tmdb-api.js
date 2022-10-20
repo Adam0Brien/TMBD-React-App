a629faebb06cf460cb75778e84a2277f4d22f0e4
Adam0Brien <20093460@mail.wit.ie>
2022-10-20 12:13:12 +0100

    Added Upcoming movies page, movies not being displayed.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -41,3 +41,14 @@ export const getMovieReviews = (id) => {
       return json.results;
     });
 };
+
+
+export const getUpcommingMovies = () => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+  )
+    .then(res => res.json())
+    .then(json => json.results);
+};
+//upcomming movies
+//https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1
