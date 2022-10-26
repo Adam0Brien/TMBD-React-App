7b6fb9f6e1d19029582210fffb0af8faa5a6ebfd
Adam0Brien <20093460@mail.wit.ie>
2022-10-26 20:15:07 +0100

    Fix upcomming movies page

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -12,7 +12,21 @@ export const getMovies = () => {
   });
 };
 
+export const getUpcommingMovies = () => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+     throw error
+  });
+};
 
+//https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1
 
 export const getMovie = (args) => {
   // console.log(args)
@@ -78,13 +92,3 @@ export const getMovieReviews = (id) => {
     });
 };
 
-
-export const getUpcommingMovies = () => {
-  return fetch(
-    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
-  )
-    .then(res => res.json())
-    .then(json => json.results);
-};
-//upcomming movies
-//https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1
