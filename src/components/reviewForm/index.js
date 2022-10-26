d6743f8c11e8b31986f49b81dbfec870ddd03b58
Adam0Brien <20093460@mail.wit.ie>
2022-10-26 20:09:53 +0100

    Fix broken stories in Storybook.

--- src/components/reviewForm/index.js
+++ src/components/reviewForm/index.js
@@ -42,13 +42,14 @@ const ReviewForm = ({ movie }) => {
     navigate("/movies/favourites");
   };
 
-    const onSubmit = (review) => {
+  const onSubmit = (review) => {
     review.movieId = movie.id;
     review.rating = rating;
-    // console.log(review);
+    console.log(review);
     context.addReview(movie, review);
     setOpen(true); // NEW
   };
+
 
   return (
     <Box component="div" sx={styles.root}>
