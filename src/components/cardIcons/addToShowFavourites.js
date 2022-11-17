76c814f8648dd291f1868dec253bf994ec03bef5
Adam0Brien <20093460@mail.wit.ie>
2022-11-17 19:51:06 +0000

    Added favourite TV show feature.

---
+++ src/components/cardIcons/addToShowFavourites.js
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { ShowsContext } from "../../contexts/showsContext";
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+
+const AddToFavourites = ({ show }) => {
+  const context = useContext(ShowsContext);
+
+  const handleAddToFavourites = (e) => {
+    e.preventDefault();
+    context.addToFavouriteShows(show);
+  };
+
+  return (
+    <IconButton aria-label="add to favorites" onClick={handleAddToFavourites}>
+      <FavoriteIcon color="primary" fontSize="large" />
+    </IconButton>
+  );
+};
+
+export default AddToFavourites;
+
